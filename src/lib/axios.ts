8c88b120bd56f4b6c1d5579039fd9c2e7ba3f6de
cauan <c.goes@gindumac.com>
2022-10-20 21:37:53 +0200

    chore: .env api

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosClient = axios.create({
-  baseURL: 'https://rickandmortyapi.com/api',
+  baseURL: process.env.NEXT_PUBLIC_RICK_API,
   headers: {
     'Content-Type': 'application/json',
   },
