46e895b28157c80dcf5cd28f2990892337462795
cauan <c.goes@gindumac.com>
2022-10-20 20:23:56 +0200

    feat: ricky and morty api

---
+++ src/lib/axios-mock.ts
@@ -0,0 +1,27 @@
+import { QueryFunction } from '@tanstack/react-query';
+import axios from 'axios';
+
+/** Add NEXT_PUBLIC_MOCK_DEPLOYMENT_URL to your production deployment on vercel! */
+const baseURL = process.env.NEXT_PUBLIC_MOCK_DEPLOYMENT_URL
+  ? `https://${process.env.NEXT_PUBLIC_MOCK_DEPLOYMENT_URL}/api/mock`
+  : process.env.NEXT_PUBLIC_VERCEL_URL
+  ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}/api/mock`
+  : 'http://localhost:3000/api/mock';
+
+export const apiMock = axios.create({
+  baseURL,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  withCredentials: false,
+});
+
+export default apiMock;
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export const mockQuery: QueryFunction<any> = async ({ queryKey }) => {
+  const [url] = queryKey;
+
+  const { data } = await apiMock.get(url as string);
+  return data;
+};
---
+++ src/lib/repositories/rickyandmortyapi.ts
@@ -0,0 +1,24 @@
+import axiosClient from '@/lib/axios';
+import {
+  ApiResponseCharacterById,
+  ApiResponseCharacters,
+  Character,
+  Location,
+} from '@/lib/types/rickyandmortyapi';
+
+export async function getCharacters(): Promise<ApiResponseCharacters> {
+  return await axiosClient.get('/character/');
+}
+// TODO: check the graphql for a query including character and same planet characters
+export async function getCharacterById(
+  id: number
+): Promise<ApiResponseCharacterById> {
+  return await axiosClient.get(`/character/${id}`);
+}
+export async function getLocationByCharacter(
+  character: Character
+): Promise<Location> {
+  return await axiosClient.get(character.location.url, {
+    baseURL: '',
+  });
+}
